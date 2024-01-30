a268b8ae80090f5422fce39129ca76a7f5d2babd
Babai-07 <khanbabai7777@gmail.com>
2024-01-30 19:19:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+/* =================== typing animation ===================== */
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Front-end Web Developer","YouTuber"],
+    typeSpeed:100,
+    BackSpeed:70,
+    loop:true
+})
