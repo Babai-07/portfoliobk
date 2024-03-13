9b43e36eacdb9126cde35ae12455a4592ca254fc
Babai-07 <khanbabai7777@gmail.com>
2024-03-13 13:08:32 +0530

    backend add and deploy

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 /* =================== typing animation ===================== */
-var typed = new Typed(".typing",{
-    strings:["","Web Designer","Web Developer","Front-end Web Developer","YouTuber"],
-    typeSpeed:100,
-    BackSpeed:70,
-    loop:true
-})
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Web Designer",
+    "Web Developer",
+    "Front-end Web Developer",
+    "YouTuber",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 70,
+  loop: true,
+});
+
+const handleSubmit = () => {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    msg: document.getElementById("msg").value,
+  });
+  console.log(raw);
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+  };
+
+  fetch(
+    "https://my-portfolio-backend-mjl9.onrender.com/api/v1/saveContact",
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error));
+};
